c9289df211a971f8962eedd5c222bcac2b4c5d1c
Michael Weingert <mpweinge@uwaterloo.ca>
2015-11-07 15:38:09 -0800

    Initial Commit

---
+++ CS224w_Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS224w_Project
+//
+//  Created by Michael Weingert on 2015-11-07.
+//  Copyright © 2015 Michael Weingert. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
