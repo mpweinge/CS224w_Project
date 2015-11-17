05c6c8294bc13d64214be55c82b74ac967697f6e
Ellen Sebastian <ellens2@stanford.edu>
2015-11-16 22:51:16 -0800

    code to calculate weighted PR over time

--- python_code/file_reader.py
+++ python_code/file_reader.py
@@ -17,17 +17,27 @@ dems2008 = {obamaTag : "Barack Obama", clintonTag: "Hilary Clinton", edwardsTag:
     bidenTag: "Joe Biden", doddTag: "Chris Dodd", gravelTag: "Mike Gravel",
     kucinichTag: "Dennis Kucinich", richardsonTag: "Bill Richardson"};
 
-
+# returns true if first date is earlier than first date.
+# e.g. dateLater("04032007", "04042007") = True.
+def dateEarlier(first, second):
+	if len(first) < 8 or len(second) < 8: return False
+	if first[-4:] < second[-4:]: return True
+	if first[-4:] > second[-4:]: return False
+	if first[0:2] < second[0:2]: return True
+	if first[0:2] > second[0:2]: return False
+	return first[2:4] < second[2:4]
 # p is the probability of adding the new node to the graph.
 # weighted is whether or not to use weights.
-def readInDonors(G, p, weighted):
+def readInDonors(G, maxDate=None):
 	weights_so_far = {} # "(n1name, n2name) -> weight"
 	num_lines = 0
 	num_negative_amount = 0
 	for line in open(donorFile):
 		num_lines += 1
-		if random.random() > p: continue
 		line = line.split('|')
+		date = line[13]
+		if maxDate is not None and not dateEarlier(date, maxDate): 
+			continue
 		committeeId = line[0]
 		name = line[7]
 		if len(name.split(",")) < 2: continue
@@ -42,10 +52,8 @@ def readInDonors(G, p, weighted):
 			key = (donorId, committeeId)
 			weights_so_far[key] = weights_so_far[key] + amount if key in weights_so_far else amount
 			G.add_weighted_edges_from([(donorId, committeeId, weights_so_far[key])])
-	print 'num lines: ', num_lines, 'num negative amount: ', num_negative_amount
-			# find donor and committee, add edge from donor to committee.
 
-def readCommitteeToCommittee(G): 
+def readCommitteeToCommittee(G, maxDate=None): 
 	num_lines = 0
 	num_negative_amount = 0
 	weights_so_far = {}
@@ -53,6 +61,9 @@ def readCommitteeToCommittee(G):
 		num_lines += 1
 		line = line.split('|')
 		id1 = line[0]
+		date = line[13]
+		if maxDate is not None and not dateEarlier(date, maxDate): 
+			continue
 		amount = int(line[14])
 		if amount < 0:
 			num_negative_amount += 1
@@ -61,4 +72,3 @@ def readCommitteeToCommittee(G):
 			key = (id1, id2)
 			weights_so_far[key] = weights_so_far[key] + amount if key in weights_so_far else amount
 			G.add_weighted_edges_from([(id1, id2, weights_so_far[key])])
-	print 'num lines: ', num_lines, 'num negative amount: ', num_negative_amount
--- python_code/weighted_pagerank.py
+++ python_code/weighted_pagerank.py
@@ -3,8 +3,33 @@ import file_reader
 
 G = nx.DiGraph() #directed graph
 
-file_reader.readInDonors(G)
-file_reader.readCommitteeToCommittee(G)
-weightedPR = nx.pagerank(G)
-for candidate in dems2008.keys():
-	print dems2008[candidate], weightedPR[candidate] 
+obamaTag = "C00431445";
+clintonTag = "C00431569";
+edwardsTag = "C00431205";
+bidenTag = "C00431916";
+doddTag = "C00431379";
+gravelTag = "C00423202";
+kucinichTag = "C00430975";
+richardsonTag = "C00431577";
+
+dems2008 = {obamaTag : "Barack Obama", clintonTag: "Hilary Clinton", edwardsTag: "John Edwards",
+    bidenTag: "Joe Biden", doddTag: "Chris Dodd", gravelTag: "Mike Gravel",
+    kucinichTag: "Dennis Kucinich", richardsonTag: "Bill Richardson"};
+
+maxDates = ['02012007','03012007','04012007','05012007','06012007','07012007','08012007','09012007','10012007','11012007','12012007','01012008']
+
+weightedPRs = {}
+for candidate in dems2008:
+	weightedPRs[candidate] = []
+
+for maxDate in maxDates:
+	file_reader.readInDonors(G, maxDate)
+	file_reader.readCommitteeToCommittee(G, maxDate)
+	print "For donations before ", maxDate
+	print "Graph size: nodes: ", str(G.number_of_nodes()), " edges: ", str(G.number_of_edges())
+	weightedPR = nx.pagerank(G)
+	for candidate in dems2008.keys():
+		weightedPRs[candidate].append(weightedPR[candidate])
+		print dems2008[candidate], weightedPR[candidate] 
+
+import matplotlib.pyplot as plt
